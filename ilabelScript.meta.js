cdba9e1ab3bd90cc87d2a00170b1610199152daf
ehekatle <ehe@live.cn>
2026-02-14 13:55:13 +0800

    Update namespace and URLs in ilabelScript.meta.js

--- ilabelScript.meta.js
+++ ilabelScript.meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         iLabel直播审核辅助
-// @namespace    https://github.com/ehekatle/ilableScript
+// @namespace    https://github.com/ehekatle/ilabel
 // @version      2.4.7
 // @description  预埋、豁免、直播信息违规、超时提示功能，集成推送功能
 // @author       ehekatle
-// @homepage     https://github.com/ehekatle/ilableScript
-// @source       https://raw.githubusercontent.com/ehekatle/ilableScript/main/ilabelScript.user.js
-// @supportURL   https://github.com/ehekatle/ilableScript/issues
-// @updateURL    https://gh-proxy.org/https://raw.githubusercontent.com/ehekatle/ilableScript/main/ilabelScript.meta.js
-// @downloadURL  https://gh-proxy.org/https://raw.githubusercontent.com/ehekatle/ilableScript/main/ilabelScript.user.js
+// @homepage     https://github.com/ehekatle/ilabel
+// @source       https://raw.githubusercontent.com/ehekatle/ilabel/main/ilabelScript.user.js
+// @supportURL   https://github.com/ehekatle/ilabel/issues
+// @updateURL    https://gh-proxy.org/https://raw.githubusercontent.com/ehekatle/ilabel/main/ilabelScript.meta.js
+// @downloadURL  https://gh-proxy.org/https://raw.githubusercontent.com/ehekatle/ilabel/main/ilabelScript.user.js
 // @match        https://ilabel.weixin.qq.com/mixed-task/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=weixin.qq.com
 // @grant        GM_xmlhttpRequest
